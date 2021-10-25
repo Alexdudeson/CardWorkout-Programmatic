830c275002df5150fc706cbad51c40d98ef23b8a
Alexey Yarov <alexxd91@gmail.com>
2021-10-25 18:36:03 +0300

    Added Cards + functional buttons

---
+++ CardWorkout-Programmatic/CardDeck.swift
@@ -0,0 +1,64 @@
+//
+//  CardDeck.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Alexey Yarov on 25/10/2021.
+//  Copyright © 2021 Alexey Yarov. All rights reserved.
+//
+
+import UIKit
+
+struct CardDeck {
+    static var allValues = [
+    UIImage(named: "icons8-2-of-clubs-100")!,
+    UIImage(named: "icons8-2-of-diamonds-100")!,
+    UIImage(named: "icons8-2-of-hearts-100")!,
+    UIImage(named: "icons8-2-of-spades-100")!,
+    UIImage(named: "icons8-3-of-clubs-100")!,
+    UIImage(named: "icons8-3-of-diamonds-100")!,
+    UIImage(named: "icons8-3-of-hearts-100")!,
+    UIImage(named: "icons8-3-of-spades-100")!,
+    UIImage(named: "icons8-4-of-clubs-100")!,
+    UIImage(named: "icons8-4-of-diamonds-100")!,
+    UIImage(named: "icons8-4-of-spades-100")!,
+    UIImage(named: "icons8-5-of-clubs-100")!,
+    UIImage(named: "icons8-5-of-diamonds-100")!,
+    UIImage(named: "icons8-5-of-hearts-100")!,
+    UIImage(named: "icons8-5-of-spades-100")!,
+    UIImage(named: "icons8-6-of-clubs-100")!,
+    UIImage(named: "icons8-6-of-diamonds-100")!,
+    UIImage(named: "icons8-6-of-hearts-100")!,
+    UIImage(named: "icons8-6-of-spades-100")!,
+    UIImage(named: "icons8-7-of-clubs-100")!,
+    UIImage(named: "icons8-7-of-diamonds-100")!,
+    UIImage(named: "icons8-7-of-hearts-100")!,
+    UIImage(named: "icons8-7-of-spades-100")!,
+    UIImage(named: "icons8-8-of-clubs-100")!,
+    UIImage(named: "icons8-8-of-diamonds-100")!,
+    UIImage(named: "icons8-8-of-hearts-100")!,
+    UIImage(named: "icons8-8-of-spades-100")!,
+    UIImage(named: "icons8-9-of-clubs-100")!,
+    UIImage(named: "icons8-9-of-diamonds-100")!,
+    UIImage(named: "icons8-9-of-hearts-100")!,
+    UIImage(named: "icons8-9-of-spades-100")!,
+    UIImage(named: "icons8-10-of-clubs-100")!,
+    UIImage(named: "icons8-10-of-diamonds-100")!,
+    UIImage(named: "icons8-10-of-hearts-100")!,
+    UIImage(named: "icons8-10-of-spades-100")!,
+    UIImage(named: "icons8-ace-of-clubs-100")!,
+    UIImage(named: "icons8-ace-of-diamonds-100")!,
+    UIImage(named: "icons8-ace-of-spades-100")!,
+    UIImage(named: "icons8-jack-of-clubs-100")!,
+    UIImage(named: "icons8-jack-of-diamonds-100")!,
+    UIImage(named: "icons8-jack-of-hearts-100")!,
+    UIImage(named: "icons8-jack-of-spades-100")!,
+    UIImage(named: "icons8-joker-100")!,
+    UIImage(named: "icons8-king-of-clubs-100")!,
+    UIImage(named: "icons8-king-of-diamonds-100")!,
+    UIImage(named: "icons8-king-of-hearts-100")!,
+    UIImage(named: "icons8-queen-of-clubs-100")!,
+    UIImage(named: "icons8-queen-of-hearts-100")!,
+    UIImage(named: "icons8-queen-of-spades-100")!
+    ]
+}
+
